01c5d6917209188b9047aa189ac15064a072bae1
Fazle Rabbe <fazlerabbee67@gmail.com>
2023-06-03 22:39:42 +0600

    first commit

---
+++ lib/pages/launcher_page.dart
@@ -0,0 +1,26 @@
+import 'package:ecom_admin_project/pages/dashboard_page.dart';
+import 'package:flutter/material.dart';
+
+import '../auth/authservice.dart';
+import 'login_page.dart';
+
+class LauncherPage extends StatelessWidget {
+  static const String routeName = '/launcher';
+  const LauncherPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration.zero, () {
+      if (AuthService.currentUser != null) {
+        Navigator.pushReplacementNamed(context, DashboardPage.routeName);
+      } else {
+        Navigator.pushReplacementNamed(context, LoginPage.routeName);
+      }
+    });
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
